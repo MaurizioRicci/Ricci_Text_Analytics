13cd6094948d68fafb94f2401db8fe93bd6dfc96
mauricci <MaurizioRicci@users.noreply.github.com>
2019-08-13 12:44:19 +0200

    code commented

--- main.py
+++ main.py
@@ -9,17 +9,18 @@ def correct(text):
     max_iterations = 10
 
     for c in range(max_iterations):
+        # creo l'input per BERT e ottengo la lista delle parole errate
         bert_input, unk_words = tokenizerForBERT.tokenize(text, debug=debug)
 
         if len(unk_words) > 0: # altrimenti nessun errore rilevato
-            predictions = BERT_model.predict(bert_input, debug=debug)
-            ordered_predictions = suggestions.evalSuggestions(predictions,
+            predictions = BERT_model.predict(bert_input, debug=debug)  # ottengo predizioni da BERT
+            ordered_predictions = suggestions.evalSuggestions(predictions,  # confronto le predizioni con l'input errato
                                                               unk_words[0], debug=debug)
             print('Best 3 suggestions:', ordered_predictions[:3])
 
             if len(ordered_predictions) > 0:
                 best_suggestion = ordered_predictions[0]['w']
-                text = text.replace(unk_words[0], best_suggestion)
+                text = text.replace(unk_words[0], best_suggestion)  # rimpiazzo la parola errata con la miglior scelta e cerco altri errori
 
                 if debug:
                     print('Corrected text:', text)
