3853b266992d9263826c90b33fcfff31de0a55e4
mauricci <MaurizioRicci@users.noreply.github.com>
2019-08-26 20:02:34 +0200

    code replacement with a map function

--- suggestions.py
+++ suggestions.py
@@ -9,19 +9,18 @@ levenshtein = Levenshtein()
 #print('Optimal:', optimalStringAlignment.distance('My goem', 'My home'))
 #print('Optimal:', optimalStringAlignment.distance('My goem', 'My go'))
 
+
 def eval_distance(test, ref):
     return levenshtein.distance(test, ref)
 
 
 def evalSuggestions(testArr, ref, debug= True):
     # valuta un array di candidati contro una parola di riferimento
-    res = []
-    for testW in testArr:
-        res.append({
-            'w': testW,
-            'dist': eval_distance(testW, ref)
-        })
-    res.sort(key=lambda k: k['dist'], reverse=False)
+    def word_ref_distance(it):
+        return dict(w=it, dist=eval_distance(it, ref))
+
+    res = map(word_ref_distance, testArr)
+    res = sorted(res, key=lambda k: k['dist'], reverse=False)
 
     if debug:
         print('Suggestions:', res)
